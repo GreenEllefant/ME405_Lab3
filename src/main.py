fe807f812920562d75663ecf3ea8c4ef6e4a03aa
c4324r <baconlord123456@gmail.com>
2023-02-07 10:10:34 -0800

    inital data sending with dummy data

--- src/main.py
+++ src/main.py
@@ -14,7 +14,8 @@ import gc
 import pyb
 import cotask
 import task_share
-
+from pyb import UART
+pyb.repl_uart(None)
 
 def task1_fun(shares):
     """!
@@ -22,14 +23,19 @@ def task1_fun(shares):
     @param shares A list holding the share and queue used by this task
     """
     # Get references to the share and queue which have been passed to this task
-    my_share, my_queue = shares
+    my_queue = shares
 
     counter = 0
     while True:
-        my_share.put(counter)
-        my_queue.put(counter)
-        counter += 1
-
+        dummy_data=[[1],[10],[0],
+                    [1],[20],[150],
+                    [1],[30],[200],
+                    [1],[40],[50],
+                    [1],[50],[-50],
+                    [1],[-1],[0]]
+        i = 0
+        my_queue.put(f"{dummy_data[0][i]},{dummy_data[1][i]},{dummy_data[2][i]}")
+        i += 1
         yield 0
 
 
@@ -39,17 +45,27 @@ def task2_fun(shares):
     @param shares A tuple of a share and queue from which this task gets data
     """
     # Get references to the share and queue which have been passed to this task
-    the_share, the_queue = shares
+    my_queue = shares
 
     while True:
         # Show everything currently in the queue and the value in the share
-        print(f"Share: {the_share.get ()}, Queue: ", end='')
-        while q0.any():
-            print(f"{the_queue.get ()} ", end='')
-        print('')
-
+        dummy_data =   [[2],[10],[0],
+                        [2],[20],[-150],
+                        [2],[30],[-200],
+                        [2],[40],[-50],
+                        [2],[50],[50],
+                        [2],[-1],[0]]
+        i = 0
+        my_queue.put(f"{dummy_data[0][i]},{dummy_data[1][i]},{dummy_data[2][i]}")
+        i += 1 
         yield 0
 
+def task3_fun(shares):
+    the_queue = shares
+    u2 = pyb.UART(2, baudrate=115200)
+    while True:
+        u2.write(the_queue.get())
+        yield 0
 
 # This code creates a share, a queue, and two tasks, then starts the tasks. The
 # tasks run until somebody presses ENTER, at which time the scheduler stops and
@@ -59,8 +75,7 @@ if __name__ == "__main__":
           "Press Ctrl-C to stop and show diagnostics.")
 
     # Create a share and a queue to test function and diagnostic printouts
-    share0 = task_share.Share('h', thread_protect=False, name="Share 0")
-    q0 = task_share.Queue('L', 16, thread_protect=False, overwrite=False,
+    q0 = task_share.Queue('B', 200, thread_protect=False, overwrite=False,
                           name="Queue 0")
 
     # Create the tasks. If trace is enabled for any task, memory will be
@@ -68,12 +83,14 @@ if __name__ == "__main__":
     # of memory after a while and quit. Therefore, use tracing only for 
     # debugging and set trace to False when it's not needed
     task1 = cotask.Task(task1_fun, name="Task_1", priority=1, period=400,
-                        profile=True, trace=False, shares=(share0, q0))
+                        profile=True, trace=False, shares=(q0))
     task2 = cotask.Task(task2_fun, name="Task_2", priority=2, period=1500,
-                        profile=True, trace=False, shares=(share0, q0))
+                        profile=True, trace=False, shares=(q0))
+    task3 = cotask.Task(task3_fun, name="Task_3", priority=3, period=3000, profile=True, trace=False, shares=(q0))
+
     cotask.task_list.append(task1)
     cotask.task_list.append(task2)
-
+    cotask.task_list.append(task3)
     # Run the memory garbage collector to ensure memory is as defragmented as
     # possible before the real-time scheduler is started
     gc.collect()
